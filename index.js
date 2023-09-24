935cddc9f337805b6cc94c35412be63123b1a672
Elisha <elishakibet67@gmail.com>
2023-09-24 11:08:31 +0300

    manipulating strings

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Elisha Kibet";
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+console.log(welcomeMessage);
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)} !`;
